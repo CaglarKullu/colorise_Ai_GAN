4d0512f439a18cd6d38a36ab2b49993fb2e1ae44
Caglar Kullu <caglarkullu@gmail.com>
2024-03-04 18:42:17 +0000

    model updated to u-net

--- model.py
+++ model.py
@@ -1,21 +1,32 @@
-from tensorflow.keras.layers import Conv2D, Conv2DTranspose, LeakyReLU, BatchNormalization, Input, Flatten, Dense
+from tensorflow.keras.layers import Conv2D, Conv2DTranspose, LeakyReLU, BatchNormalization, Input, Concatenate, Flatten, Dense
 from tensorflow.keras.models import Model
 
 def build_generator():
     inputs = Input(shape=(32, 32, 1))
-    # Encoder starts
-    x = Conv2D(32, kernel_size=3, strides=1, padding="same")(inputs)
-    x = LeakyReLU(alpha=0.2)(x)
-    x = BatchNormalization()(x)
-    # Adjust or remove upsampling here to ensure output is 32x32x3
-    # For example, you might skip the upsampling or use a different strategy
-    # Output layer
-    outputs = Conv2D(3, kernel_size=3, padding="same", activation="tanh")(x)  # Ensure this outputs 32x32x3
+
+    # Encoder
+    c1 = Conv2D(32, kernel_size=3, strides=1, padding="same")(inputs)
+    c1 = LeakyReLU(alpha=0.2)(c1)
+    c1 = BatchNormalization()(c1)
     
+    c2 = Conv2D(64, kernel_size=3, strides=2, padding="same")(c1)
+    c2 = LeakyReLU(alpha=0.2)(c2)
+    c2 = BatchNormalization()(c2)
+    
+    # Decoder
+    c3 = Conv2DTranspose(64, kernel_size=3, strides=2, padding="same")(c2)
+    c3 = LeakyReLU(alpha=0.2)(c3)
+    c3 = BatchNormalization()(c3)
+    
+    # Skip Connection
+    c3 = Concatenate()([c3, c1])
+    
+    # Output layer
+    outputs = Conv2D(3, kernel_size=3, padding="same", activation="tanh")(c3)  # Ensure this outputs 32x32x3
     
     model = Model(inputs, outputs)
-    print(model.summary())
     return model
+
 
 def build_discriminator():
     inputs = Input(shape=(32, 32, 3))
